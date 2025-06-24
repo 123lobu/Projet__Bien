9106d2a8b51dbd353b0dcc123d096ea695517bfa
123lobu <henocklobu12@gmail.com>
2025-06-25 00:03:20 +0200

    connection

--- PROJET/pages/inscit.php
+++ PROJET/pages/inscit.php
@@ -1,10 +1,10 @@
 <?php
     //récupération des données du formulaire
     $nom = htmlspecialchars($_POST['fullname']);
-    $postnom = htmlspecialchars($_POST['Post-nom']);
+    $postnom = htmlspecialchars($_POST['Postnom']);
     $email = htmlspecialchars($_POST['email']);
-    $password = htmlspecialchars($_POST['password']);
-    $date = htmlspecialchars($_POST['date']);
+    $motdepasse = htmlspecialchars($_POST['motdepasse']);
+    $dates = htmlspecialchars($_POST['dates']);
     $genre = htmlspecialchars($_POST['genre']);
 
     //connexion à la base de données
@@ -16,12 +16,12 @@
         die('Erreur: ' .$e->getMessage());
     }
     //requête sql
-    $req = $bdd->prepare("INSERT INTO incription (nom, postnom, email, password, date_naissance, genre) VALUES (:nom, :postnom, :email, :password, :date, :genre)");
+    $req = $bdd->prepare("INSERT INTO incription (nom, postnom, email, motdepasse, date_naissance, genre) VALUES (:nom, :postnom, :email, :motdepasse, :dates, :genre)");
     $req->bindParam(':nom', $nom);
     $req->bindParam(':postnom', $postnom);
     $req->bindParam(':email', $email);
-    $req->bindParam(':password', $password);
-    $req->bindParam(':date', $date);
+    $req->bindParam(':motdepasse', $motdepasse);
+    $req->bindParam(':dates', $dates);
     $req->bindParam(':genre', $genre);
     $req->execute();
 ?> 
